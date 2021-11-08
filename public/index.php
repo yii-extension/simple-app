a9eec8b57bd4bea12a4c32db001a259dde8ccf7e
Wilmer <terabytesoftw@gmail.com>
2021-11-08 12:15:16 -0300

    Implement `yiisoft/runner` packages.

--- public/index.php
+++ public/index.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-use Simple\App\Runner\WebApplicationRunner;
+use Yiisoft\Yii\Runner\Web\WebApplicationRunner;
 
 // PHP built-in server routing.
 if (PHP_SAPI === 'cli-server') {
-    // Serve static files as is.
-    if (is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    /** @var string */
+    $requestUri = $_SERVER['REQUEST_URI'] ?? '';
+
+    if (is_file(__DIR__ . parse_url($requestUri, PHP_URL_PATH))) {
         return false;
     }
 
